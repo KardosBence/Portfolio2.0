d58832c98895dcb7a85dfb9fa7ea872c0042748c
kardosb <kardbence@gmail.com>
2023-04-04 14:35:47 +0200

    V1.0.0

---
+++ script.ts
@@ -0,0 +1,48 @@
+const aktYear = new Date().getFullYear();
+aktYear.innerHTML = aktYear;
+/*let aktDatum: string = formatDate(new Date());
+
+function padTo2Digits(num: number) {
+    return num.toString().padStart(2, '0');
+}
+
+function formatDate(aktDatum: Date) {
+    return (
+        [
+            aktDatum.getFullYear(),
+            padTo2Digits(aktDatum.getMonth() + 1),
+            padTo2Digits(aktDatum.getDate()),
+        ].join('-') +
+        ' ' +
+        [
+            padTo2Digits(aktDatum.getHours()),
+            padTo2Digits(aktDatum.getMinutes()),
+            padTo2Digits(aktDatum.getSeconds()),
+        ].join(':')
+    );
+}
+
+const aktDatumElem = document.getElementById("aktDatum");
+
+setInterval(() => {
+    aktDatum = formatDate(new Date());
+    aktDatumElem.innerHTML = "Mai dátum: " + aktDatum;
+}, 1000);*/
+
+var koszontesElem: HTMLElement = document.getElementById("koszontes");
+koszontesElem.innerHTML = "Szia!";
+
+function KoszontesBeallitas() {
+  const ora: number = new Date().getHours();
+  let koszontes: string;
+  if (ora < 9) {
+    koszontes = "Jó reggelt!";
+  } else if (ora < 18) {
+    koszontes = "Szia!";
+  } else {
+    koszontes = "Jó estét!";
+  }
+  koszontesElem.innerHTML = koszontes;
+}
+
+setInterval(KoszontesBeallitas, 1000);
