7c38823b39ce7eee5239c0311ddbab1c169d8bc4
kardosb <kardbence@gmail.com>
2023-04-05 15:24:37 +0200

    V1.0.3 Hamburger Style

--- script.js
+++ script.js
@@ -65,8 +65,3 @@ backToTopButton.addEventListener("click", function () {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 });
-function menuOnClick() {
-    document.getElementById("menu-bar").classList.toggle("change");
-    document.getElementById("nav").classList.toggle("change");
-    document.getElementById("menu-bg").classList.toggle("change-bg");
-}
