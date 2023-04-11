d7e448cf52f1dcca0f969e88073af838655b0508
kardosb <kardbence@gmail.com>
2023-04-11 14:33:53 +0200

    V1.0.4 added jquery

--- script.ts
+++ script.ts
@@ -69,3 +69,22 @@ backToTopButton.addEventListener("click", function() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 });
+/*Nav-link Active Jquery Start*/
+$(document).ready(function(){
+  $('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top - 80
+        }, 100);
+    }
+  });
+});
+$(document).ready(function(){
+  $('a[href^="#"]').on('click', function(event) {
+    $('nav a.nav-link').removeClass('active');
+    $(this).addClass('active');
+  });
+});
+/*Nav-link Active Jquery End*/
