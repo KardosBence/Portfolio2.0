3e5fa8234e34a1fc53bacac5ed5563ef61275bbd
kardosb <kardbence@gmail.com>
2023-04-17 15:13:26 +0200

    V1.0.5. New Projekt elements

--- script.js
+++ script.js
@@ -1,34 +1,7 @@
 var aktYear = new Date().getFullYear();
 var aktYearElem = document.getElementById("aktYear");
 aktYearElem.innerHTML = aktYear;
-/*let aktDatum: string = formatDate(new Date());
 
-function padTo2Digits(num: number) {
-    return num.toString().padStart(2, '0');
-}
-
-function formatDate(aktDatum: Date) {
-    return (
-        [
-            aktDatum.getFullYear(),
-            padTo2Digits(aktDatum.getMonth() + 1),
-            padTo2Digits(aktDatum.getDate()),
-        ].join('-') +
-        ' ' +
-        [
-            padTo2Digits(aktDatum.getHours()),
-            padTo2Digits(aktDatum.getMinutes()),
-            padTo2Digits(aktDatum.getSeconds()),
-        ].join(':')
-    );
-}
-
-const aktDatumElem = document.getElementById("aktDatum");
-
-setInterval(() => {
-    aktDatum = formatDate(new Date());
-    aktDatumElem.innerHTML = "Mai dátum: " + aktDatum;
-}, 1000);*/
 var koszontesElem = document.getElementById("koszontes");
 koszontesElem.innerHTML = "Szia!";
 function KoszontesBeallitas() {
@@ -45,23 +18,27 @@ function KoszontesBeallitas() {
     }
     koszontesElem.innerHTML = koszontes;
 }
+
 setInterval(KoszontesBeallitas, 1000);
+
 document.querySelector('.navbar-button').addEventListener('click', function () {
     document.querySelector('.animated-icon').classList.toggle('open');
 });
-var backToTopButton = document.querySelector("#back-to-top");
-window.onscroll = function () {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        backToTopButton.style.display = "block";
-    }
-    else {
-        backToTopButton.style.display = "none";
-    }
-};
-backToTopButton.addEventListener("click", function () {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+
+var backToTopButton = document.getElementById("back-to-top");
+
+window.addEventListener("scroll", function() {
+  if (window.pageYOffset > 100) { // Ha az ablak tetejétől mért távolság nagyobb, mint 100px
+    backToTopButton.style.display = "block"; // Akkor megjelenik a gomb
+  } else {
+    backToTopButton.style.display = "none"; // Ellenkező esetben elrejtődik
+  }
 });
+
+backToTopButton.addEventListener("click", function() {
+  window.scrollTo(0, 0); // A dokumentum tetejére animálja az ablakot
+});
+
 /*Nav-link Active Jquery Start*/
 $(document).ready(function () {
     $('a[href^="#"]').on('click', function (event) {
